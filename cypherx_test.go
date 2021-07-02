7896d6e94b1692b7f613a1b008891ef1449f9e95
shu-bc <shu@baseconnect.in>
2021-07-02 17:34:54 +0900

    test scan

--- cypherx_test.go
+++ cypherx_test.go
@@ -25,7 +25,8 @@ func TestGet(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	db.SendQuery("merge (:Person{name: 'peter', age: 30,  salary: 1000.1})", map[string]interface{}{})
+	db.SendQuery("match (p:Person{name: 'peter'}) delete p", map[string]interface{}{})
+	db.SendQuery("merge (:Person{name: 'peter', age: 30,  salary: 1000.1, social_id: '123abc'})", map[string]interface{}{})
 	p := &Person{}
 	err = db.Get(p, "match (p:Person{name: 'peter'}) return p", map[string]interface{}{})
 	if err != nil {
@@ -35,4 +36,6 @@ func TestGet(t *testing.T) {
 	assert.Equal(t, "peter", p.Name)
 	assert.Equal(t, 30, p.Age)
 	assert.Equal(t, 1000.1, p.Salary)
+	assert.True(t, p.SocialID.Valid)
+	assert.Equal(t, "123abc", p.SocialID.String)
 }
