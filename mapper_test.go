de8c708a07e08279598a55834d244cd236d2efcc
shu-bc <shu@baseconnect.in>
2021-07-10 21:30:19 +0900

    improve mapper test

--- mapper_test.go
+++ mapper_test.go
@@ -17,8 +17,6 @@ func TestIsValidPtr(t *testing.T) {
 }
 
 func TestGenerateAssignmentFunc(t *testing.T) {
-	// 必ずstructのポインターからフィールドを取得する必要があります
-	// reflect.Value の CanAddr()の条件を参照
 	s := &struct {
 		Name        sql.NullString
 		Description string
@@ -31,90 +29,102 @@ func TestGenerateAssignmentFunc(t *testing.T) {
 		}
 	}{}
 
-	// Name
-	fv := reflect.ValueOf(s).Elem().Field(0)
-	f, err := generateAssignmentFunc(fv.Type())
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("scanner type field", func(t *testing.T) {
+		fv := reflect.ValueOf(s).Elem().Field(0)
+		f, err := generateAssignmentFunc(fv.Type())
+		if err != nil {
+			t.Error(err)
+		}
+		if err := f(fv, "name"); err != nil {
+			t.Error(err)
+		}
+		assert.True(t, s.Name.Valid)
+		assert.Equal(t, "name", s.Name.String)
+	})
 
-	if err := f(fv, "aaa"); err != nil {
-		t.Fatal(err)
-	}
-	assert.True(t, s.Name.Valid)
-	assert.Equal(t, "aaa", s.Name.String)
+	t.Run("string field", func(t *testing.T) {
+		fv := reflect.ValueOf(s).Elem().Field(1)
+		f, err := generateAssignmentFunc(fv.Type())
+		if err != nil {
+			t.Error(err)
+		}
 
-	// Desc
-	fv = reflect.ValueOf(s).Elem().Field(1)
-	f, err = generateAssignmentFunc(fv.Type())
-	if err != nil {
-		t.Fatal(err)
-	}
+		if err := f(fv, "description"); err != nil {
+			t.Error(err)
+		}
+		assert.Equal(t, "description", s.Description)
 
-	if err := f(fv, "description"); err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, "description", s.Description)
-	err = f(fv, 123)
-	assert.Error(t, err)
+		err = f(fv, 123)
+		assert.Error(t, err)
+	})
 
-	// Age
-	fv = reflect.ValueOf(s).Elem().Field(2)
-	f, err = generateAssignmentFunc(fv.Type())
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("int field", func(t *testing.T) {
+		fv := reflect.ValueOf(s).Elem().Field(2)
+		f, err := generateAssignmentFunc(fv.Type())
+		if err != nil {
+			t.Error(err)
+		}
 
-	if err := f(fv, int64(10)); err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, 10, s.Age)
+		if err := f(fv, int64(10)); err != nil {
+			t.Error(err)
+		}
+		assert.Equal(t, 10, s.Age)
+		// must pass int64 type value
+		assert.Error(t, f(fv, 1))
+	})
 
-	// Height
-	fv = reflect.ValueOf(s).Elem().Field(3)
-	f, err = generateAssignmentFunc(fv.Type())
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("float field", func(t *testing.T) {
+		fv := reflect.ValueOf(s).Elem().Field(3)
+		f, err := generateAssignmentFunc(fv.Type())
+		if err != nil {
+			t.Error(err)
+		}
 
-	if err := f(fv, float64(100)); err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, float64(100), s.Height)
+		if err := f(fv, float64(100)); err != nil {
+			t.Error(err)
+		}
+		assert.Equal(t, float64(100), s.Height)
+		assert.Error(t, f(fv, float32(100)))
+	})
 
-	// Alive
-	fv = reflect.ValueOf(s).Elem().Field(4)
-	f, err = generateAssignmentFunc(fv.Type())
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("bool field", func(t *testing.T) {
+		fv := reflect.ValueOf(s).Elem().Field(4)
+		f, err := generateAssignmentFunc(fv.Type())
+		if err != nil {
+			t.Error(err)
+		}
 
-	if err := f(fv, true); err != nil {
-		t.Fatal(err)
-	}
-	assert.True(t, s.Alive)
+		if err := f(fv, true); err != nil {
+			t.Error(err)
+		}
+		assert.True(t, s.Alive)
+	})
 
-	// Relative
-	fv = reflect.ValueOf(s).Elem().Field(5)
-	f, err = generateAssignmentFunc(fv.Type())
-	if err != nil {
-		t.Fatal(err)
-	}
-	node := dbtype.Node{
-		Props: map[string]interface{}{
-			"name": "relative",
-			"age":  int64(12),
-		},
-	}
-	if err := f(fv, node); err != nil {
-		t.Fatal(err)
-	}
-	expect := struct {
-		Name string
-		Age  int
-	}{
-		Name: "relative",
-		Age:  12,
-	}
-	assert.Equal(t, expect, s.Relative)
+	t.Run("struct field", func(t *testing.T) {
+		fv := reflect.ValueOf(s).Elem().Field(5)
+		f, err := generateAssignmentFunc(fv.Type())
+		if err != nil {
+			t.Error(err)
+		}
+		node := dbtype.Node{
+			Props: map[string]interface{}{
+				"name": "relative",
+				"age":  int64(12),
+			},
+		}
+		if err := f(fv, node); err != nil {
+			t.Error(err)
+		}
+		expect := struct {
+			Name string
+			Age  int
+		}{
+			Name: "relative",
+			Age:  12,
+		}
+		assert.Equal(t, expect, s.Relative)
+		// check mapper cache
+		_, ok := typeMapperCache.mapping[reflect.TypeOf(s.Relative)]
+		assert.True(t, ok)
+	})
 }
